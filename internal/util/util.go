044319adc14ca609808c36f1d607f1c22764530f
GodOfPro <1387ilia@gmail.com>
2024-05-03 23:11:10 +0330

    remove unused func

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,7 +3,6 @@ package util
 import (
 	"log/slog"
 	"net"
-	"strings"
 )
 
 func WriteToConnection(header string, conn net.Conn) {
@@ -11,11 +10,4 @@ func WriteToConnection(header string, conn net.Conn) {
 	if err != nil {
 		slog.Error("Failed to write to the connection: ", err)
 	}
-}
-
-func stripPath(str string) string {
-	s := strings.Split(string(str), " ")
-	content, _ := strings.CutPrefix(s[1], "/echo/")
-
-	return content
 }
