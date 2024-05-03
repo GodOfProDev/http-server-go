049c2fd9847d919bd738ff775b74d4972aae9308
GodOfPro <1387ilia@gmail.com>
2024-05-03 22:43:52 +0330

    remove unused code

--- app/server.go
+++ app/server.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log/slog"
 	"net"
-	"os"
 	"regexp"
 	"strings"
 )
@@ -157,79 +154,4 @@ func (s *Server) handleConnection(conn net.Conn) {
 	writeToConnection(response.String(), conn)
 
 	return
-
-	header := "HTTP/1.1 "
-	isEcho := false
-	isUserAgent := false
-	isFileGet := false
-	isFilePost := false
-	var fullFilePath string
-
-	if req.Path == "/" {
-		header += "200 OK\r\n\r\n"
-	} else if strings.Contains(req.Path, "/echo") {
-		header += "200 OK\r\n"
-		isEcho = true
-
-	} else if strings.Contains(req.Path, "/user-agent") {
-		header += "200 OK\r\n"
-		isUserAgent = true
-	} else if strings.Contains(req.Path, "/files") && req.Method == "GET" {
-		fileName, _ := strings.CutPrefix(req.Path, "/files/")
-		fullFilePath = s.Directory + fileName
-
-		_, err := os.Stat(fullFilePath)
-
-		if os.IsNotExist(err) {
-			header += "404 Not Found\r\n\r\n"
-		} else {
-			header += "200 OK\r\n"
-			isFileGet = true
-		}
-	} else if strings.Contains(req.Path, "/files") && req.Method == "POST" {
-		fileName, _ := strings.CutPrefix(req.Path, "/files/")
-		fullFilePath = s.Directory + fileName
-
-		header += "201 Created\r\n"
-		isFilePost = true
-	} else {
-		header += "404 Not Found\r\n\r\n"
-	}
-
-	if strings.Contains(header, "\r\n\r\n") {
-		writeToConnection(header, conn)
-		return
-	}
-
-	if isEcho {
-		strippedPath := stripPath(data)
-
-		header += "Content-Type: text/plain\r\n"
-		header += fmt.Sprintf("Content-Length: %d\r\n\r\n", len(strippedPath))
-		header += strippedPath + "\r\n\r\n"
-	} else if isUserAgent {
-		header += "Content-Type: text/plain\r\n"
-		header += fmt.Sprintf("Content-Length: %d\r\n\r\n", len(req.UserAgent))
-		header += req.UserAgent
-	} else if isFileGet {
-		file, _ := os.Open(fullFilePath)
-		defer file.Close()
-
-		fileContents, _ := io.ReadAll(file)
-		fileContentsStr := string(fileContents)
-
-		header += "Content-Type: application/octet-stream\r\n"
-		header += fmt.Sprintf("Content-Length: %d\r\n\r\n", len(fileContentsStr))
-		header += fileContentsStr
-	} else if isFilePost {
-		file, _ := os.Create(fullFilePath)
-		defer file.Close()
-
-		data := []byte(req.Body)
-		file.Write(data)
-
-		header += "\r\n"
-	}
-
-	writeToConnection(header, conn)
 }
